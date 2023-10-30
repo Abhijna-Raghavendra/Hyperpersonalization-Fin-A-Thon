17a9ace07bb4ed7c12c8b53aeac85e05c437000a
SVyusti <vyusti2083@gmail.com>
2023-10-31 03:53:42 +0530

    feat:add postgres

--- server/src/views.py
+++ server/src/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,HttpResponse
+from .serializers import *
+from .models import *
+from rest_framework import viewsets
 
 # Create your views here.
 
 def hello(request):
     return HttpResponse("HELLO WORLD")
+
+class DumyModelViewSet(viewsets.ModelViewSet):
+    queryset=Dumy.objects.all()
+    serializer_class=DumySerializer
