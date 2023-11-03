cd4e6ec8b757028d5b3db3f6097eabfb6c0f1eb1
SVyusti <vyusti2083@gmail.com>
2023-11-03 10:31:30 +0530

    feat:add models,views

--- server/src/serializers.py
+++ server/src/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import *
 
-class DumySerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Dumy
+        model=User
+        fields="__all__"
+
+class CreditScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CreditScore
+        fields="__all__"
+
+class ExpensesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Expenses
         fields="__all__"
