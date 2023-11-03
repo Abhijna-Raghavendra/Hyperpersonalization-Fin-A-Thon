c30863c8e236da680c3f0420cece56ea8c8f8d65
SVyusti <vyusti2083@gmail.com>
2023-11-03 10:59:08 +0530

    feat: add models,views

--- server/src/urls.py
+++ server/src/urls.py
@@ -4,8 +4,8 @@ from  .views import *
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
-router.register(r"dumy",DumyModelViewSet,basename="dumy")
+router.register(r"User",UserModelViewSet,basename="User")
+router.register(r"CreditScore",CreditScoreModelViewSet,basename="CreditScore")
+router.register(r"Expenses",ExpensesModelViewSet,basename="Expenses")
 
-urlpatterns=[
-    path('server/',hello),
-]+router.urls
+urlpatterns=router.urls
